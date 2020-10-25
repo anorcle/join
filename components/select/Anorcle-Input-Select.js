ffb78f8fa21b88f709f02a61dd399c026d8d2d00
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2020-10-25 15:12:11 +0530

    added select option in Anorcle Float Input

--- components/select/Anorcle-Input-Select.js
+++ components/select/Anorcle-Input-Select.js
@@ -1,3 +1,8 @@
+const downArrow = `<svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7 12L0.504809 0.75L13.4952 0.75L7 12Z" fill="black"/>
+</svg>
+`
+
 class AnorcleInputSelect extends HTMLElement {
     constructor() {
         super()
@@ -7,14 +12,28 @@ class AnorcleInputSelect extends HTMLElement {
         const options = this.getAttribute("options").split(",").map(opt => opt.trim())
 
         const optionContainer = document.createElement("div")
+        optionContainer.classList.add("option-container")
         options.forEach(optionText => {
             const option = document.createElement("div")
             option.innerText = optionText
             option.classList.add("option")
             optionContainer.append(option)
         })
-        optionContainer.classList.add("option-container")
-        this.append(optionContainer)
+
+        const selectInput = document.createElement("div")
+        selectInput.classList.add("select-input")
+
+        const selectedText = document.createElement("div")
+        selectedText.classList.add("selected-text")
+        selectedText.innerText = "Select"
+
+        const iconContainer = document.createElement("div")
+        iconContainer.classList.add("icon-container")
+        iconContainer.innerHTML = downArrow
+
+        selectInput.append(selectedText, iconContainer)
+
+        this.append(selectInput, optionContainer)
     }
 }
 customElements.define('anorcle-input-select', AnorcleInputSelect);
